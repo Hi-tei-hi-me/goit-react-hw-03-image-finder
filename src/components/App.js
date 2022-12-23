5144faa90ee8fc859a60536736ee5b3cbd57dfbe
Hi-tei-hi-me <lethargium@gmail.com>
2022-12-23 21:43:37 +0200

    Basic markup

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const PIXABAY_KEY = '30808706-03d9568f6e15a5d7585f5680b';
+// `https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`
+
+export class App extends Component {
+  render(
+)}
